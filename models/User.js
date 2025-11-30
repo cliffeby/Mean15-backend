5a4662ef3ce14ac8f9c83ed8676563e873f0e9f8
cliffeby <cliffeby@gmail.com>
2025-11-30 09:16:37 -0500

    changed id to _id; moved user interface to models

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, select: false },
   role: { type: String, enum: ['admin', 'user'], default: 'user' },
-  defaultLeague: { type: String, default: 'Premier' } // Persist user's league selection
+  defaultLeague: { type: String, default: 'Rochester' } // Persist user's league selection
 }, { timestamps: true });
 
 userSchema.pre('save', async function (next) {
